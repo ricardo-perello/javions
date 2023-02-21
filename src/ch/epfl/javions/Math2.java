942b67380382530c2d37f7fc4226a0df3dd4ee5d
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-02-21 18:38:32 +0100

    Math2 class added (clamp and asinh)

---
+++ src/ch/epfl/javions/Math2.java
@@ -0,0 +1,34 @@
+package ch.epfl.javions;
+
+public class Math2 {
+    private Math2(){}
+
+    /**
+     * Clamp: limits value v to interval min-max
+     * @param min
+     * @param v
+     * @param max
+     * @return v if v lies between min and max.
+     * if v lower than min, return min.
+     * if v greater than max, return max.
+     * @throws IllegalArgumentException if min greater than max
+     */
+    public static int clamp(int min, int v, int max){
+        if (min > max){
+            throw new IllegalArgumentException();
+        }
+        if (v < min) return min;
+        else if (v > max) return max;
+        else return v;
+    }
+
+    /**
+     *
+     * Arcsinh of x
+     * @param x
+     * @return arcsinh of x
+     */
+    public static double asinh(double x){
+    return ((Math.log(x+(Math.sqrt(1+(x*x))))));
+    }
+}
