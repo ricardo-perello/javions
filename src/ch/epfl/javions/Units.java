ff74255dd565fe0954583f7d2710cf6ce44ba331
Alejandro Meredith Romero <alejandro.meredithromero@epfl.ch>
2023-02-21 14:54:10 +0100

    UNITS FINICHED

--- src/ch/epfl/javions/Units.java
+++ src/ch/epfl/javions/Units.java
@@ -1,13 +1,17 @@
 package ch.epfl.javions;
 
 public class Units {
-    private Units(){}
-//Alejandro ES MUY PUTA
+    private Units() {
+    }
+
+    //Alejandro ES MUY PUTA
     public static final double CENTI = 1e-2;
     public static final double KILO = 1e3;
 
     public static class Length {
-        private Length(){}
+        private Length() {
+        }
+
         public static final double METER = 1;
         public static final double CENTIMETER = CENTI * METER;
         public static final double KILOMETER = KILO * METER;
@@ -16,24 +20,42 @@ public class Units {
         public static final double NAUTICAL_MILE = 1852 * METER;
     }
 
-    public static class Angle{
-        private Angle(){}
+    public static class Angle {
+        private Angle() {
+        }
+
         public static final double RADIAN = 1;
         public static final double TURN = 2 * Math.PI * RADIAN;
         public static final double DEGREE = TURN / 360;
-        public static final double T32 = TURN / 2e32;
+        public static final double T32 = TURN / Math.scalb(1, 32);
     }
 
-    public static class Time{
-        private Time(){}
+    public static class Time {
+        private Time() {
+        }
+
         public static final double SECOND = 1;
         public static final double MINUTE = 60 * SECOND;
         public static final double HOUR = 60 * MINUTE;
     }
 
     public static class Speed {
-        private Speed(){}
+        private Speed() {
+        }
+
         public static final double KNOT = Length.NAUTICAL_MILE / Time.HOUR;
         public static final double KILOMETER_PER_HOUR = Length.KILOMETER / Time.HOUR;
+    }
+
+    public static double convert(double value, double fromUnit, double toUnit) {
+        return value * (fromUnit / toUnit);
+    }
+
+    public static double convertFrom(double value, double fromUnit) {
+        return value * fromUnit;
+    }
+
+    public static double convertTo(double value, double toUnit) {
+        return value * (1 / toUnit);
     }
 }
