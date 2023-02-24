e91a034e0837fb0548c780460b84e6bf761a9334
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-02-24 17:50:12 +0100

    AircraftTypeDesignator

--- src/ch/epfl/javions/aircraft/AircraftTypeDesignator.java
+++ src/ch/epfl/javions/aircraft/AircraftTypeDesignator.java
@@ -1,10 +1,11 @@
 package ch.epfl.javions.aircraft;
 import java.util.regex.Pattern;
+
 import static ch.epfl.javions.Preconditions.checkArgument;
 
-public record IcaoAddress(String string) {
-    static Pattern check = Pattern.compile("[0-9A-F]{6}");
-    public IcaoAddress{
+public record AircraftTypeDesignator(String string) {
+    static Pattern check = Pattern.compile("[A-Z0-9]{2,4}");
+    public AircraftTypeDesignator{
         checkArgument(check.matcher(string).matches());
     }
 }
