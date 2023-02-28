b8d6c4b66539f339f139b8ebd6451b7a320116f9
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-02-28 13:20:06 +0100

    crc24 optimized class

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -5,17 +5,28 @@ import static ch.epfl.javions.Bits.extractUInt;
 public final class Crc24 {
     public static int GENERATOR = 0xFFF409;
     private final int generator;
-    private static int mask = 16_777_215;
-    byte[] Crc24Table;
+    private static final int mask = 16_777_215;
     public Crc24 (int generator){
         this.generator = generator;
+    }
+
+    public int crc(int generator, byte[] bytes){
+        int[] table = buildTable(generator);
+        int crc = 0;
+        for (byte b: bytes){
+            crc = ((crc << 8)|Byte.toUnsignedInt(b))^table[extractUInt(crc, 16,8 )];
+        }
+        for (int i=0;i<4;++i){
+            crc = ((crc << 8))^table[extractUInt(crc, 16,8 )];
+        }
+        crc = crc & mask;
+
+        return crc;
     }
 
     public static int crc_bitwise(int generator, byte[] bytes){
         int[] table = {0, generator & mask};
         int crc = 0;
-        int byteIndex;
-        int bitIndex;
         for(byte b: bytes){
             for(int i=7;i>=0;i--){
                 crc = ((crc << 1) | extractUInt(b,i,1))^ table[extractUInt(crc, 23, 1)];
@@ -31,8 +42,8 @@ public final class Crc24 {
     }
 
 
-    public int crc_Basic (){
-        return crc_bitwise(generator, Crc24Table);
+    public int crc_Basic (int generator, byte[] bytes){
+        return crc_bitwise(generator, bytes);
     }
 
     private static int[] buildTable(int generator){
@@ -42,4 +53,6 @@ public final class Crc24 {
         }
         return table;
     }
+
+
 }
