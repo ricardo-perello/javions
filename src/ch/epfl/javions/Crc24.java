6ff02a90f36ed33d0b4787b37a88e89b5161274b
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-03-01 16:27:58 +0100

    added comments for Crc24

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -7,11 +7,18 @@ public final class Crc24 {
     private final int generator;
     private static final int mask = 16777215;
     private final int [] table;
+
+
     public Crc24 (int generator){
         this.generator = generator & mask;
         table = buildTable(this.generator);
     }
-//todo add comments
+
+    /**
+     * Optimized crc process (bytes)
+     * @param bytes byte array containing the bit string
+     * @return decoded crc
+     */
     public int crc(byte[] bytes){
         int crc = 0;
         for (byte b: bytes){
@@ -25,6 +32,12 @@ public final class Crc24 {
         return crc;
     }
 
+    /**
+     * Optimized crc process (bits)
+     * @param generator crc generator
+     * @param bytes byte array containing the bit string
+     * @return decoded crc
+     */
     public static int crc_bitwise(int generator, byte[] bytes){
         int[] table = {0,generator & mask};
         int crc = 0;
@@ -41,12 +54,22 @@ public final class Crc24 {
         crc = crc & mask;
         return crc;
     }
-    //~16 000 000 000
 
+    /**
+     * Basic crc that uses crc_bitwise.
+     * @param generator crc generator
+     * @param bytes byte array containing the bit string
+     * @return decoded crc
+     */
     public int crc_Basic (int generator, byte[] bytes){
         return crc_bitwise(generator, bytes);
     }
 
+    /**
+     * Builds table with a given generator
+     * @param generator crc generator
+     * @return int table with all the values of the generator
+     */
     private static int[] buildTable(int generator){
         int [] table = new int[256];
         for (int i = 0; i < 256; i++) {
