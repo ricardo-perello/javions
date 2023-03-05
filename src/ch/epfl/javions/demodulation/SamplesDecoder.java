aad29a9ff9be90f723ef2766ae4f1534f0459129
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-03-05 23:05:02 +0100

    nearly finished SamplesDecoder

--- src/ch/epfl/javions/demodulation/SamplesDecoder.java
+++ src/ch/epfl/javions/demodulation/SamplesDecoder.java
@@ -1,6 +1,5 @@
 package ch.epfl.javions.demodulation;
 
-import ch.epfl.javions.Bits;
 import ch.epfl.javions.Preconditions;
 
 import java.io.IOException;
@@ -10,26 +9,37 @@ import java.util.Objects;
 public final class SamplesDecoder {
     private InputStream stream;
     private int batchSize;
-    private byte[] sampleDecoder;
+    private byte[] littleEndian;
+    private int[] bigEndian;
 
     public SamplesDecoder(InputStream stream, int batchSize) {
         Objects.requireNonNull(stream);
         Preconditions.checkArgument(batchSize > 0);
         this.stream = stream;
         this.batchSize = batchSize;
-        sampleDecoder = new byte[batchSize * 2];
+        littleEndian = new byte[batchSize * 2];
+        bigEndian = new int[batchSize * 2];
     }
 
     public int readBatch(short[] Batch) throws IOException {
         Preconditions.checkArgument(Batch.length == batchSize);
         int lengthStream = stream.available();
-        int nBytesRead = stream.readNBytes(sampleDecoder, 0, batchSize);
+        int nBytesRead = stream.readNBytes(littleEndian, 0,2 * batchSize);
         for (int i = 0; i < batchSize; i++) {
-            byte littleEdian = (byte) (sampleDecoder[2 * i] << 8);
-            byte bigEdian = (byte) (sampleDecoder[2 * i + 1] >>> 8);
-            Batch[i] = (short) (littleEdian | bigEdian);
+            bigEndian[2*i] = littleEndian[2*i+1];
+            bigEndian[2*i+1] = littleEndian[2*i];
+            System.out.println(bigEndian[2*i]<<8);
+            Batch[i] = (short) (((bigEndian[2*i]<<8)|bigEndian[2*i+1])-2048);
         }
-        if (lengthStream == sampleDecoder.length) {
+        /*
+        for (int i = 0; i < batchSize; i++) {
+            byte littleEndian = (byte) (sampleDecoder[2 * i] << 8);
+            byte bigEndian = (byte) (sampleDecoder[2 * i + 1] >>> 8);
+            Batch[i] = (short) (littleEndian | bigEndian);
+        }
+
+         */
+        if (lengthStream == littleEndian.length) {
             return batchSize / 2;
         } else {
             return (int) Math.floor(lengthStream / 2);
