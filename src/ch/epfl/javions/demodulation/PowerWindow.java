e5fc86ffdc215cd918da0fc4bfc5de1e3ff1d4b0
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-03-08 12:18:06 +0100

    first version of PowerWindow

--- src/ch/epfl/javions/demodulation/PowerWindow.java
+++ src/ch/epfl/javions/demodulation/PowerWindow.java
@@ -6,31 +6,71 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public final class PowerWindow {
+    final static int POW16 = (int) Math.pow(2, 16);
+    final static int POW17 = (int) Math.pow(2, 17);
     private InputStream stream;
     private int windowSize;
+    private int windowPosition;
     private PowerComputer powerComputer;
+    private int[] even = new int[POW16];
+    private int[] odd = new int[POW16];
+    private int[] currentArray = even;
+    boolean isEvenTable = true;
+    private int numOfSamples;
+
 //todo add comments
     public PowerWindow(InputStream stream, int windowSize) throws IOException {
-        Preconditions.checkArgument((windowSize > 0) && (windowSize <= Math.scalb(1, 16)));
+        Preconditions.checkArgument((windowSize > 0) && (windowSize <= POW16));
         this.stream = stream;
         this.windowSize = windowSize;
-        powerComputer = new PowerComputer(stream, windowSize); // TODO: 7/3/23 check if this is correct
+        powerComputer = new PowerComputer(stream, POW17);
+        numOfSamples = powerComputer.readBatch(even);
     }
 
     public int size(){return windowSize;}
 
-    public long position(){}
+    public long position(){return windowPosition;}
 
-    public boolean isFull() {}
+    public boolean isFull() {return (windowPosition+windowSize < numOfSamples);}
 
     public int get(int i){
         if (!((i >= 0)&&(i < windowSize))){
             throw new IndexOutOfBoundsException();
         }
+        if (windowPosition + i < numOfSamples){
+            return currentArray[windowPosition+i];
+        }
+        else{
+            int newIndex = numOfSamples-(windowPosition+i);
+            switchBoolean(isEvenTable);
+            return currentArray[newIndex];
+        }
     }
 
-    public void advance() throws IOException{}
+    public void advance() throws IOException{
+        windowPosition++;
+        if (windowPosition + windowSize > numOfSamples) {
+            switchBoolean(isEvenTable);
+            windowPosition = 0;
+            numOfSamples = powerComputer.readBatch(currentArray);
+        }
 
-    public void advanceBy(int offset) throws IOException{}
+    }
+
+    public void advanceBy(int offset) throws IOException{
+        for (int i = 0; i < offset; i++) {
+            advance();
+        }
+    }
+    private void switchBoolean(boolean b){
+        if (b){
+            b = false;
+            currentArray = odd;
+        }
+        else{
+            b = true;
+            currentArray = even;
+        }
+    }
 
 }
