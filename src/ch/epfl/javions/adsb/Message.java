996a7199468c4fc6f218b89cf6f0de2d7ba0cdaa
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-03-17 14:55:15 +0100

    added comments for Message and added Ricardo token

--- src/ch/epfl/javions/adsb/Message.java
+++ src/ch/epfl/javions/adsb/Message.java
@@ -3,7 +3,17 @@ package ch.epfl.javions.adsb;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
 public interface Message {
-    //TODO comentarios
+
+    /**
+     * Abstract method timestamp
+     * @return the timestamp of the message, in nanoseconds
+     *
+     */
     long timeStampNs();
+
+    /**
+     * Abstract method icaoAddress
+     * @return the ICAO address of the sender of the message.
+     */
     IcaoAddress icaoAddress();
 }
