a6d208609edbdf4d9982315237cb846a51d5d05b
Alejandro Meredith Romero <alejandro.meredithromero@epfl.ch>
2023-03-17 14:59:00 +0100

    submit

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -34,7 +34,7 @@ public final class Submit {
     // Jeton du premier membre du groupe
     private static final String TOKEN_1 = "deiH4ahc";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "tie2Eilo";
+    private static final String TOKEN_2 = "jaih5AeZ";
     // -------------
 
     private static final String ZIP_ENTRY_NAME_PREFIX = "Javions/";
