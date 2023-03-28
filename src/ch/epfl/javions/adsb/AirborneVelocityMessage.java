ed352686c5ca9a40413c2d733986f62190aa3faf
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-03-28 11:38:02 +0200

    AirborneVelocityMessage finished + sigcheck 6

--- src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -26,39 +26,48 @@ public record AirborneVelocityMessage (long timeStampNs, IcaoAddress icaoAddress
 
         //different cases of subtype
 
-        switch(subtype){
-            case 1,2:
+        switch (subtype) {
+            case 1, 2 -> {
                 //direction
-                int Dew = Bits.extractUInt(contentOfMessage,21,1);
-                int Dns = Bits.extractUInt(contentOfMessage,10,1);
+                int Dew = Bits.extractUInt(contentOfMessage, 21, 1);
+                int Dns = Bits.extractUInt(contentOfMessage, 10, 1);
                 //velocity
-                int Vew = Bits.extractUInt(contentOfMessage,11,10) - 1;
-                int Vns = Bits.extractUInt(contentOfMessage,0,10) - 1;
-                if (Vew == 0 || Vns == 0) return null;
+                int Vew = Bits.extractUInt(contentOfMessage, 11, 10) - 1;
+                int Vns = Bits.extractUInt(contentOfMessage, 0, 10) - 1;
+                if (Vew == -1 || Vns == -1) return null;
                 //speed
-                double speed = Math.hypot(Vew, Vns);
+                double vel = Math.hypot(Vew, Vns);
                 //angle
-                int x = (Dew == 0) ? Vew : Vew * -1 ;
-                int y = (Dns == 0) ? Vns : Vns * -1 ;
-                double trackOrHeading =  Math.atan2(x,y);
-                trackOrHeading = (trackOrHeading < 0) ? (2*Math.PI)+trackOrHeading : trackOrHeading;
-                trackOrHeading = Units.convertTo(trackOrHeading,DEGREE );
+                int x = (Dew == 0) ? Vew : Vew * -1;
+                int y = (Dns == 0) ? Vns : Vns * -1;
+                double dir = Math.atan2(x, y);
+                dir = (dir < 0) ? (2 * Math.PI) + dir : dir;
+                dir = Units.convertTo(dir, DEGREE);
                 //adjusting for case
-                if (subtype == 2) speed+=4;
-                break;
-            case 3,4:
-               int headingAvailable = Bits.extractUInt(contentOfMessage, 21, 1);
-               if (headingAvailable == 1) {
-                   double heading = (Bits.extractUInt(contentOfMessage, 11, 10)/Math.pow(2,10));
-               }
+                if (subtype == 2) vel += 4;
+                return new AirborneVelocityMessage(timeStampNs,icaoAddress,vel,dir);
+            }
+            case 3, 4 -> {
+                int headingAvailable = Bits.extractUInt(contentOfMessage, 21, 1);
+                //direction
+                double heading;
+                if (headingAvailable == 1) {
+                    heading = (Bits.extractUInt(contentOfMessage, 11, 10) / Math.pow(2, 10));
+                    heading = Units.convertTo(heading, DEGREE);
+                }
+                else {return null;}
 
-
-
-                break;
-            default:
+                //speed
+                double airspeed = Bits.extractUInt(contentOfMessage, 0, 10) - 1;
+                if (airspeed == -1) return null;
+                //adjusting for case
+                airspeed = (subtype == 3) ? airspeed : airspeed * 4;
+                return new AirborneVelocityMessage(timeStampNs,icaoAddress,airspeed,heading);
+            }
+            default -> {
                 return null;
-                // TODO: 28/3/23 check if this is true
+            }
         }
-        return new AirborneVelocityMessage(timeStampNs,icaoAddress,speed,trackOrHeading);
+
     }
 }
