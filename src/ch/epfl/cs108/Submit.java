aad282103d98aa41a1b662f28a83db61959f3754
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-04-06 19:25:05 +0200

    Submit

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -32,7 +32,7 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "aegoiSh5";
+    private static final String TOKEN_1 = "OhG6arix";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
     private static final String TOKEN_2 = "ieth2eeL";
     // -------------
