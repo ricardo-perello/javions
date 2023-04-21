0fb9ee9b49757344fc568b7ea23f61966b3a4468
Alejandro Meredith Romero <alejandro.meredithromero@epfl.ch>
2023-04-21 17:43:50 +0200

    test

---
+++ test/ch/epfl/javions/gui/ObservavbleAircraftStateTest.java
@@ -0,0 +1,29 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.ByteString;
+import ch.epfl.javions.adsb.RawMessage;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.IIOException;
+import java.io.*;
+
+public class ObservavbleAircraftStateTest {
+    @Test
+    void test1() throws IIOException {
+        try (DataInputStream s = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream("resources/messages_20230318_0915.bin")))){
+            byte[] bytes = new byte[RawMessage.LENGTH];
+            while (true) {
+                long timeStampNs = s.readLong();
+                int bytesRead = s.readNBytes(bytes, 0, bytes.length);
+                assert bytesRead == RawMessage.LENGTH;
+                ByteString message = new ByteString(bytes);
+                System.out.printf("%13d: %s\n", timeStampNs, message);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
