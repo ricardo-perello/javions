496bcb7861957346fd033bc5a27d4df67f28267d
Alejandro Meredith Romero <alejandro.meredithromero@epfl.ch>
2023-04-23 18:05:13 +0200

    MapParameters

---
+++ src/ch/epfl/javions/gui/MapParameters.java
@@ -0,0 +1,36 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.Preconditions;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+public final class MapParameters {
+    private ObjectProperty<Integer> zoom;
+    private ObjectProperty<Double> minX;
+    private ObjectProperty<Double> minY;
+
+    public MapParameters(int zoom, double minX, double minY) {
+        Preconditions.checkArgument(zoom >= 6 && zoom <= 19 &&
+                                    minX < Math.scalb(2, 8 + zoom) && minX >= 0 &&
+                                    minY < Math.scalb(2, 8 + zoom) && minY >= 0);
+        this.zoom = new SimpleObjectProperty<>(zoom);
+        this.minX = new SimpleObjectProperty<>(minX);
+        this.minY = new SimpleObjectProperty<>(minY);
+    }
+
+    public void scroll(double x, double y) {
+        double newMinX = minX.getValue() + x;
+        double newMinY = minY.getValue() + y;
+        Preconditions.checkArgument(newMinX >= 0 && newMinX <Math.scalb(2, 8+ zoom.getValue()) &&
+                newMinY >= 0 && newMinY < Math.scalb(2, 8+ zoom.getValue()));
+        //todo condiciones x y
+        minX.set(minX.getValue() + x);
+        minY.set(minY.getValue() + y);
+    }
+
+    public void changeZoomLevel(int zoomChange) {
+        int newValueZoom = zoom.getValue() + zoomChange;
+        Preconditions.checkArgument(newValueZoom >= 6 && newValueZoom <= 19);
+        zoom.set(newValueZoom);
+    }
+}
