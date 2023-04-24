90b8b0d40d064ca4567b2bffed8e0716ece265f9
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-04-24 17:34:28 +0200

    TileManager tile handling works

--- src/ch/epfl/javions/gui/TileManager.java
+++ src/ch/epfl/javions/gui/TileManager.java
@@ -1,94 +1,100 @@
 package ch.epfl.javions.gui;
 
-import ch.epfl.javions.Math2;
-import ch.epfl.javions.Preconditions;
 import javafx.scene.image.Image;
 
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.Map;
-
 
 
 public final class TileManager {
 
-    public record TileId(int zoom, int x, int y){
-        public TileId{
-            Preconditions.checkArgument(isValid(zoom(),x(),y()));
-        }
-        public static boolean isValid(int zoom, int x, int y) {
+    private static final int CACHE_MAX_ENTRIES = 100;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75F;
+    private static final String PROTOCOL_STRING = "https://";
+    private static final String FILE_ENDING = ".png";
+    private static final String PATH_SEPARATOR = "/";
 
-            if (zoom >= 6 && zoom <= 19 &&
-                    x >= 0 && x < Math.scalb(4, zoom) &&
-                    y >= 0 && y < Math.scalb(4, zoom)){
-                return true;
-            }
-            return false;
-        }
-    }
-    private static final int MAX_ENTRIES = 100;
     private final Path path;
     private final String serverName;
-    private final LinkedHashMap<TileId, Image> memoryCache =
-            new LinkedHashMap<TileId, Image>(100, 1, true){
-                @Override
-                protected boolean removeEldestEntry(Map.Entry<TileId, Image> eldest) {
-                    return this.size() > MAX_ENTRIES;
-                }
+    private final LinkedHashMap<TileId, Image> memoryCache;
 
 
-    };
+    public record TileId(int zoom, int x, int y) {
+        public TileId {
+            //Preconditions.checkArgument(isValid(zoom(), x(), y())); this doesnt work (big x)
+        }// TODO: 24/4/23 check if we need conditions
+
+        public static boolean isValid(int zoom, int x, int y) {
+
+            return zoom >= 6 && zoom <= 19 &&
+                    x >= 0 && x < Math.scalb(4, zoom) &&
+                    y >= 0 && y < Math.scalb(4, zoom);
+        }
+    }
 
 
-    public TileManager(Path path, String serverName){
+    public TileManager(Path path, String serverName) {
         this.path = path;
-        this.serverName = serverName;
+        this.serverName = PROTOCOL_STRING + serverName;
+        memoryCache =
+                new LinkedHashMap<>(CACHE_MAX_ENTRIES, DEFAULT_LOAD_FACTOR, true) {
+
+                };
     }
 
     public Image imageForTileAt(TileId tileId) throws IOException {
-        String directoryString = "/" + tileId.zoom() + "/" + tileId.x();
-        String tileString = directoryString + "/" + tileId.y() + ".png";
-        Path tilePath = Path.of(path.toString(), tileString);
-        if (memoryCache.containsKey(tileId)){
+        String serverString = serverName + PATH_SEPARATOR + tileId.zoom() + PATH_SEPARATOR
+                + tileId.x() + PATH_SEPARATOR + tileId.y() + FILE_ENDING;
+
+        String directoryString = path.toString() + PATH_SEPARATOR + tileId.zoom()
+                + PATH_SEPARATOR + tileId.x();
+
+        String tileString = directoryString + PATH_SEPARATOR + tileId.y() + FILE_ENDING;
+        Path directoryPath = Path.of(directoryString);
+        Path tilePath = Path.of(tileString);
+
+        if (memoryCache.containsKey(tileId)) {
             System.out.println("Image found in memory cache!");
             return memoryCache.get(tileId);
         }
-        else if (Files.exists(tilePath) ){
+        if (Files.exists(tilePath)) {
             byte[] bytes = Files.readAllBytes(tilePath);
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-            Image image = new Image(inputStream);
-            memoryCache.put(tileId, image);
+            Image image = new Image(new ByteArrayInputStream(bytes));
+            updateCache(tileId, image);
             System.out.println("Image found in disk cache!");
             return image;
         }
-        else{
-            try {
-                URL u  =  new  URL (serverName + tileString );
-                URLConnection c  = u.openConnection();
-                c.setRequestProperty( "User-Agent" , "I had" );
-                InputStream  i  = c.getInputStream();
-                byte[] bytes = i.readAllBytes();
+
+        URL u = new URL(serverString);
+        URLConnection c = u.openConnection();
+        c.setRequestProperty("User-Agent", "I had");
+        try (InputStream i = c.getInputStream()) {
+
+            byte[] bytes = i.readAllBytes();
 
 
-                Image image = new Image(new ByteArrayInputStream(bytes));
-                memoryCache.put(tileId, image);
-                System.out.println("Image retrieved from server!");
-                Files.createDirectories(Path.of(path + directoryString));
-                Files.createFile(tilePath);
-                Files.write(tilePath, bytes);
-                return image;
+            Image image = new Image(new ByteArrayInputStream(bytes));
+            updateCache(tileId, image);
+            System.out.println("Image retrieved from server!");
+            Files.createDirectories(directoryPath);
+            Files.write(tilePath, bytes);
+            return image;
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+    }
+
+    private void updateCache(TileId tileId, Image image) {
+        if(memoryCache.size() >= CACHE_MAX_ENTRIES){
+            memoryCache.remove(memoryCache.keySet().iterator().next());
+        }
+        memoryCache.put(tileId,image);
     }
 }
--- test/ch/epfl/javions/gui/TestTileManager.java
+++ test/ch/epfl/javions/gui/TestTileManager.java
@@ -1,6 +1,7 @@
 package ch.epfl.javions.gui;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +13,14 @@ public final class TestTileManager extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         TileManager t = new TileManager(Path.of("tile-cache"),
-                "https://tile.openstreetmap.org");
-        t.imageForTileAt(new TileManager.TileId(17, 67927, 46357));
-        for (int i = 0; i < 120; i++) {
-            t.imageForTileAt(new TileManager.TileId(17, i, 46357));
+                "tile.openstreetmap.org");
+        Image i = t.imageForTileAt(new TileManager.TileId(17, 67927, 46357));
+        Image j = t.imageForTileAt(new TileManager.TileId(17, 67927, 46357));
+        for (int k = 0; k < 150; k++) {
+            t.imageForTileAt(new TileManager.TileId(17, 67927+k, 46357));
         }
-        for (int i = 0; i < 120; i++) {
-            t.imageForTileAt(new TileManager.TileId(17, i, 46357));
+        for (int k = 150; k > 0; k--) {
+            t.imageForTileAt(new TileManager.TileId(17, 67927+k, 46357));
         }
         Platform.exit();
     }
