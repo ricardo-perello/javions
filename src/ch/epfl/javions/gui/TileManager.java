a41856bcf9223a5b489ff982ed753d51299b6931
Ricardo Perelló Mas <ricardo.perellomas@epfl.ch>
2023-04-26 17:35:26 +0200

    comments added to TileManager

--- src/ch/epfl/javions/gui/TileManager.java
+++ src/ch/epfl/javions/gui/TileManager.java
@@ -28,12 +28,34 @@ public final class TileManager {
     private final String serverName;
     private final LinkedHashMap<TileId, Image> memoryCache;
 
-
+    /**
+     * TileId record is an identification for a specific tile of the map
+     *
+     * @param zoom zoom level
+     * @param x    x position of tile
+     * @param y    y position of tile
+     */
     public record TileId(int zoom, int x, int y) {
+
+        /**
+         * Compact constructor of TileId that calls isValid method.
+         *
+         * @param zoom zoom level
+         * @param x    x position of tile
+         * @param y    y position of tile
+         */
         public TileId {
             Preconditions.checkArgument(isValid(zoom, x, y));
-        }// TODO: 24/4/23 check if we need conditions
+        }
 
+        /**
+         * isValid method checks if the parameters of the tile can exist.
+         *
+         * @param zoom zoom level
+         * @param x    x position of tile
+         * @param y    y position of tile
+         * @return true when tile is valid, false if tile is not valid (tile doesn't exist).
+         */
         public static boolean isValid(int zoom, int x, int y) {
 
             return zoom >= MIN_ZOOM_LEVEL && zoom <= MAX_ZOOM_LEVEL &&
@@ -42,7 +64,12 @@ public final class TileManager {
         }
     }
 
-
+    /**
+     * Constructor for TileManager
+     *
+     * @param path       path of repository where the tiles will be stored in disk.
+     * @param serverName name of the server where you will request the tiles from. (tile.openstreetmap.org)
+     */
     public TileManager(Path path, String serverName) {
         this.path = path;
         this.serverName = PROTOCOL_STRING + serverName;
@@ -52,6 +79,16 @@ public final class TileManager {
                 };
     }
 
+    /**
+     * ImageForTileAt method is in charge of finding the image of a tile. It looks for the image in
+     * your memory cache first, if it doesn't find it there, it looks in the disk cache, if it
+     * doesn't find it there it will request it using HTTP from the server and store it in the caches.
+     *
+     * @param tileId is TileId of the tile we want the image of.
+     * @return Image of tile.
+     * @throws IOException when stream has a problem during the reading or writing of the image
+     *                     from the caches.
+     */
     public Image imageForTileAt(TileId tileId) throws IOException {
         String serverString = serverName + PATH_SEPARATOR + tileId.zoom() + PATH_SEPARATOR
                 + tileId.x() + PATH_SEPARATOR + tileId.y() + FILE_ENDING;
@@ -90,6 +127,13 @@ public final class TileManager {
 
     }
 
+    /**
+     * UpdateCache ensures that the memory cache doesn't surpass its maximum capacity when you
+     * add a new tile to the cache.
+     *
+     * @param tileId tileId of the tile you want to put in the cache.
+     * @param image image of the tile you want to put in the cache.
+     */
     private void updateCache(TileId tileId, Image image) {
         if (memoryCache.size() >= CACHE_MAX_ENTRIES) {
             memoryCache.remove(memoryCache.keySet().iterator().next());
